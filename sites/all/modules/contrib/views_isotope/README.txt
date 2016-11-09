d856a7b2f39d3f6bfb5964331a0b3082423a1d6e
Robert Cordes <rcordes@gmail.com>
2016-11-09 12:44:59 -0600

    views_isotope upate

--- sites/all/modules/contrib/views_isotope/README.txt
+++ sites/all/modules/contrib/views_isotope/README.txt
@@ -1,19 +1,7 @@
 Views Isotope
 -------------
-Views Isotope is a views style plugin that allows views to return results as jQuery Isotope styled structure.
+Views Isotope implements the javascript isotope package by metafizzy.
 
-## Dependencies
-* views
-* jquery_update
-* libraries
+Documentation: https://www.drupal.org/node/2586191
 
-## Installation
-* Download the latest version of [jquery.isotope.min.js][jquery.isotope]
-* Place jquery.isotope.min.js in sites/all/libraries/jquery.isotope/
-
-### Note
-The D7 version of this module was based on a D6 version found in @funkym's sandbox
-The D6 code was build using methods borrowed from views_cycle
-
-[jquery.isotope]: http://isotope.metafizzy.co/jquery.isotope.min.js
-[views_cycle]: http://drupal.org/project/views_cycle
+Installation Instructions: https://www.drupal.org/node/2634162
--- sites/all/modules/contrib/views_isotope/views_isotope.js
+++ sites/all/modules/contrib/views_isotope/views_isotope.js
@@ -1,42 +1,181 @@
-(function($) {
-  $(document).ready(function() {
-  
-  var $container = $('#isotope-container');
-      
-  $container.isotope({
-    itemSelector: '.isotope-element',
-    //filter: '.nothing'
-  });
-  
-  var $optionSets = $('#isotope-options .option-set'),
-      $optionLinks = $optionSets.find('a');
- 
-      $optionLinks.click(function(){
-        
-        var $this = $(this);
-        // don't proceed if already selected
-        if ( $this.hasClass('selected') ) {
-          return false;
+/**
+ * @file
+ * Drupal-specific implementation of isotope package.
+ */
+
+(function ($) {
+  var isotopeActiveFilters = {};
+
+  function params_unserialize(p) {
+    if (typeof p == 'undefined') {
+      return {};
+    }
+    var ret = {},
+        seg = p.replace(/^\?/,'').split('&'),
+        len = seg.length, i = 0, s;
+    for (; i < len; i++) {
+      if (!seg[i]) {
+        continue;
+      }
+      s = seg[i].split('=');
+      ret[s[0]] = s[1];
+    }
+    return ret;
+  }
+
+  function isotope_add_hash(group, value, instanceID) {
+    var hash = window.location.href.split('#')[1];
+    var params = params_unserialize(hash);
+    if (typeof instanceID != 'undefined') {
+      params[group + '.' + instanceID] = value;
+    }
+    else {
+      params[group] = value;
+    }
+    location.hash = $.param(params);
+  }
+
+  function isotope_hash() {
+    var hash = window.location.href.split('#')[1];
+    var params = params_unserialize(hash);
+    $.each(params, function(key, value) {
+      var filterGroup = key.split('.')[0];
+      var instanceID = key.split('.')[1];
+      if (filterGroup == 'sort') {
+        var $selector = $(".isotope-options.sorts");
+        if (instanceID) {
+          $selector = $selector.filter("[data-instance-id='" + instanceID + "']");
         }
-        var $optionSet = $this.parents('.option-set');
-        $optionSet.find('.selected').removeClass('selected');
-        $this.addClass('selected');
-  
-        var options = {},
-            key = $optionSet.attr('data-option-key'),
-            value = $this.attr('data-option-value');
-        // parse 'false' as false boolean
-        value = value === 'false' ? false : value;
-        options[ key ] = value;
-        if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
-          // changes in layout modes need extra logic
-          changeLayoutMode( $this, options )
-        } else {
-          // otherwise, apply new options
-          $container.isotope( options );
+        if ($selector.length) {
+          // This sort option exists.
+          isotope_apply('sort', value, instanceID);
         }
-        
-        return false;
-      });
- });
-})(jQuery);
+      }
+      else {
+        var $selector = $(".isotope-options[data-filter-group='" + filterGroup + "']");
+        if (instanceID) {
+          var $selector = $selector.filter("[data-instance-id='" + instanceID + "']");
+        }
+        if ($selector.length) {
+          // This filter option exists.
+          isotope_apply('filter', value, instanceID, filterGroup);
+        }
+      }
+    });
+  }
+
+  function isotope_apply(type, value, instanceID, filterGroup) {
+    // Find all identical optionSets.
+    var $optionSets = $(".isotope-options");
+    if (type == 'filter') {
+      $optionSets = $optionSets.filter("[data-filter-group='" + filterGroup + "']");
+    }
+    if (type == 'sort') {
+      $optionSets = $optionSets.filter(".sorts");
+    }
+    var $container = $('.isotope-container');
+    if (typeof instanceID != 'undefined') {
+      var $optionSets = $optionSets.filter("[data-instance-id='" + instanceID + "']");
+      var $container = $('#' + instanceID);
+    }
+
+    // Apply class change to all identical optionsets.
+    $optionSets.find('.selected').removeClass('selected');
+    if (type == 'filter') {
+      $optionSets.find("[data-filter='" + value + "']").addClass('selected');
+      // Combine filters.
+      var applyValue = '';
+      isotopeActiveFilters[filterGroup] = value;
+      for (var prop in isotopeActiveFilters) {
+        applyValue += isotopeActiveFilters[ prop ];
+      }
+      $container.isotope({ filter: applyValue });
+    }
+    if (type == 'sort') {
+      $optionSets.find("[data-sort-by='" + value + "']").addClass('selected');
+      applyValue = value.split(',');
+      $container.isotope({ sortBy: applyValue });
+    }
+  }
+
+  Drupal.behaviors.views_isotope = {
+    attach:function(context, settings)
+    {
+      if ($('body', context).length == 0) {
+        // This is an ajax call, likely a prepend.
+        var $new_items = $(context).filter('.isotope-element');
+        if ($new_items.length) {
+          $('.isotope-container').isotope('appended', $new_items);
+        };
+      }
+
+      var $container = $('.isotope-container', context);
+      if ($container.length) {
+        // Pre-select first option in option sets.
+        $('.isotope-options > li:first-child > a', context).addClass('selected');
+
+        // Use imagesLoaded if it is available.
+        if (typeof imagesLoaded !== 'undefined') {
+          var container = $container.get();
+          imagesLoaded(container, function() {
+            if ($container.data('isotope')) {
+              $container.isotope('layout');
+            }
+          });
+        }
+
+        // Config options that are not instance-specific.
+        var config = $container.data('isotope-options');
+
+        // Check for new hash values.
+        if (config.urlFilters == 1) {
+          window.onhashchange = isotope_hash;
+          isotope_hash();
+        }
+
+        if (config.stamp) {
+          $container.isotope().isotope('stamp', $(config.stamp));
+        }
+
+        // Filter button click.
+        $('.isotope-options', context).delegate('.filterbutton:not(.selected)', 'click', function(e) {
+          var $this = $(this);
+
+          // Identify what has been clicked.
+          var $optionSet = $this.parents('.isotope-options');
+          var filterGroup = $optionSet.attr('data-filter-group');
+          var instanceID = $optionSet.attr('data-instance-id');
+          var filterValue = $this.attr('data-filter');
+
+          if (config.urlFilters == 1) {
+            isotope_add_hash(filterGroup, filterValue, instanceID);
+          }
+          else {
+            isotope_apply('filter', filterValue, instanceID, filterGroup);
+          }
+
+          e.preventDefault();
+        });
+
+        // Sort button click.
+        $('.isotope-options', context).delegate('.sorterbutton:not(.selected)', 'click', function(e) {
+          var $this = $(this);
+
+          // Identify what has been clicked.
+          var $optionSet = $this.parents('.isotope-options');
+          var instanceID = $optionSet.attr('data-instance-id');
+          var sortValue = $this.attr('data-sort-by');
+
+          if (config.urlFilters == 1) {
+            isotope_add_hash('sort', sortValue, instanceID);
+          }
+          else {
+            isotope_apply('sort', sortValue, instanceID);
+          }
+
+          e.preventDefault();
+        });
+      }
+    }
+  }
+}(jQuery));
